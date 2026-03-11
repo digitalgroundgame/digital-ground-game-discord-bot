05356abed11b4525c2f7938c9770a57e4a12d814
Paul Searcy <tridon777@gmail.com>
2026-03-11 12:47:29 -0500

    Removing view date joined and sent commands

--- src/commands/metadata.ts
+++ src/commands/metadata.ts
@@ -103,26 +103,11 @@ export const ChatCommandMetadata: {
 
 export const MessageCommandMetadata: {
   [command: string]: RESTPostAPIContextMenuApplicationCommandsJSONBody
-} = {
-  VIEW_DATE_SENT: {
-    type: ApplicationCommandType.Message,
-    name: Lang.getRef('messageCommands.viewDateSent', Language.Default),
-    name_localizations: Lang.getRefLocalizationMap('messageCommands.viewDateSent'),
-    default_member_permissions: undefined,
-    dm_permission: true,
-  },
-}
+} = {}
 
 export const UserCommandMetadata: {
   [command: string]: RESTPostAPIContextMenuApplicationCommandsJSONBody
 } = {
-  VIEW_DATE_JOINED: {
-    type: ApplicationCommandType.User,
-    name: Lang.getRef('userCommands.viewDateJoined', Language.Default),
-    name_localizations: Lang.getRefLocalizationMap('userCommands.viewDateJoined'),
-    default_member_permissions: undefined,
-    dm_permission: true,
-  },
   SEND_DEV_ONBOARDING: {
     type: ApplicationCommandType.User,
     name: Lang.getRef('userCommands.sendDevOnboarding', Language.Default),
